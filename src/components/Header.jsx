6819f79381ca70186eeeef90d82c86efd3aa1b36
Pradeep <pradeepkr.bhk@gmail.com>
2024-11-20 12:59:56 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='p-2 shadow-2xl'>
+        <div className='max-w-[1200px] mx-auto border border-red-500'>
+            Hello
+        </div>
+
+    </header>
+  )
+}
